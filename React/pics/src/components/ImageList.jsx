a3d2ba6b92d03c00452aaec7ec8ec8057f1aa7c2
Sayan Dafadar <dafadarts@gmail.com>
2022-11-23 19:50:34 +0530

    web

--- React/pics/src/components/ImageList.jsx
+++ React/pics/src/components/ImageList.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import ImageShow from "./ImageShow";
+import "./ImageList.css";
 
 function ImageList({ imageData }) {
   return (
-    <>
+    <div className="image-list">
       {imageData.map((image) => (
         <ImageShow key={image.id} imageUrl={image.urls.thumb} />
       ))}
-    </>
+    </div>
   );
 }
 
--- React/pics/src/components/SearchBar.jsx
+++ React/pics/src/components/SearchBar.jsx
@@ -1,16 +1,22 @@
 import React, { useState } from "react";
+import "./SearchBar.css";
 
 function SearchBar({ onSubmit }) {
   const [term, setTerm] = useState("");
-  
+
   const handleSubmit = (event) => {
     event.preventDefault();
     onSubmit(term);
   };
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onInput={(e) => setTerm(e.target.value)} />
+      <form className="search-bar-form" onSubmit={handleSubmit}>
+        <input
+          className="search-bar"
+          placeholder="Search"
+          type="text"
+          onInput={(e) => setTerm(e.target.value)}
+        />
       </form>
     </div>
   );
